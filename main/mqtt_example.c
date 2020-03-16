b2d92aaa6e8e1bc89b3561bd0cfb7824e8aa703e
kevin <kevinwojtyszyn@gmail.com>
2020-03-16 13:18:57 -0300

    Ordenamiento de codigo

--- main/mqtt_example.c
+++ main/mqtt_example.c
@@ -23,6 +23,7 @@
 #include "../headers/mqtt.h"
 #include "../headers/node.h"
 #include "../headers/mconfig.h"
+#include "../headers/nvs.h"
 
 void LectPir (void *pvParameter);
 
@@ -112,82 +113,7 @@ void LectPir (void *pvParameter)
     vTaskDelete(NULL);
 }
 
-esp_err_t get_mwifi_nvs (mwifi_config_t * cfg, char * cuarto)
-{
-	nvs_handle my_handle;
-	esp_err_t err;
 
-	// Open
-	err = nvs_open("MWIFI_CONF", NVS_READONLY, &my_handle);
-	if (err != ESP_OK) return err;
-
-	// Read
-	size_t required_size;
-	err = nvs_get_str(my_handle,"router_ssid",cfg->router_ssid,&required_size);
-	if (err != ESP_OK && err != ESP_ERR_NVS_NOT_FOUND) return err;
-
-	err = nvs_get_str(my_handle,"router_pass",cfg->router_password,&required_size);
-	if (err != ESP_OK && err != ESP_ERR_NVS_NOT_FOUND) return err;
-
-	err = nvs_get_str(my_handle,"mesh_pass",cfg->mesh_password,&required_size);
-	if (err != ESP_OK && err != ESP_ERR_NVS_NOT_FOUND) return err;
-
-	char *aux;
-
-	err = nvs_get_str(my_handle,"room",NULL,&required_size);
-	aux=malloc(sizeof(char)*(required_size+1));
-	err = nvs_get_str(my_handle,"room",aux,&required_size);
-	if (err != ESP_OK && err != ESP_ERR_NVS_NOT_FOUND) return err;
-
-	strcpy(cuarto,aux);
-	free(aux);
-
-	size_t size = sizeof(cfg->mesh_id);
-	nvs_get_blob(my_handle, "mesh_id", cfg->mesh_id, &size);
-
-	// Close
-	nvs_close(my_handle);
-	return ESP_OK;
-}
-
-esp_err_t set_mwifi_nvs (mwifi_config_t * cfg,char * cuarto)
-{
-	nvs_handle my_handle;
-	esp_err_t err;
-
-	// Open
-	err = nvs_open("MWIFI_CONF", NVS_READWRITE, &my_handle);
-	if (err != ESP_OK) return err;
-
-	// Write
-	err = nvs_set_str(my_handle,"router_ssid",cfg->router_ssid);
-	if (err != ESP_OK && err != ESP_ERR_NVS_NOT_FOUND) return err;
-
-	err = nvs_set_str(my_handle,"router_pass",cfg->router_password);
-	if (err != ESP_OK && err != ESP_ERR_NVS_NOT_FOUND) return err;
-
-
-	err = nvs_set_str(my_handle,"mesh_pass",cfg->mesh_password);
-	if (err != ESP_OK && err != ESP_ERR_NVS_NOT_FOUND) return err;
-
-	err = nvs_set_str(my_handle,"room",cuarto);
-	if (err != ESP_OK && err != ESP_ERR_NVS_NOT_FOUND) return err;
-
-	size_t size = sizeof(cfg->mesh_id);
-	nvs_set_blob(my_handle, "mesh_id", cfg->mesh_id, size);
-	if (err != ESP_OK && err != ESP_ERR_NVS_NOT_FOUND) return err;
-
-    // Commit written value.
-    // After setting any values, nvs_commit() must be called to ensure changes are written
-    // to flash storage. Implementations may write to storage at other times,
-    // but this is not guaranteed.
-    err = nvs_commit(my_handle);
-    if (err != ESP_OK) return err;
-
-	// Close
-	nvs_close(my_handle);
-	return ESP_OK;
-}
 
 esp_err_t config_mesh (mwifi_config_t * cfg)
 {
